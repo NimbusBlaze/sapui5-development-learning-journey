8364e100dbd1a4ca358d97c6913138812334e246
Shahriyar Hossain <inbox.shahriyar@gmail.com>
2025-08-11 14:56:59 +0000

    XML View

--- webapp/index.js
+++ webapp/index.js
@@ -1,6 +1,18 @@
-sap.ui.define(["sap/m/Text"], function (Text) {
+sap.ui.define(["sap/ui/core/mvc/XMLView"], function (XMLView) {
   "use strict";
 
-  new Text({ text: "Hello World" }).placeAt("content");
+  XMLView.create({
+    id: "App",
+    viewName: "sap.training.exc.view.App"
+  }).then(function (oView) {
+    oView.placeAt("content");
+  });
 
 });
+
+// sap.ui.define(["sap/m/Text"], function (Text) {
+//   "use strict";
+
+//   new Text({ text: "Hello World" }).placeAt("content");
+
+// });
